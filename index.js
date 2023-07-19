337ded870e96f34b734771cef797f3e868ccb731
hnhoa2000 <huynhngochoa2000@gmail.com>
2023-07-19 14:23:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import http from 'http';
+import { Server } from 'socket.io';
+import { engine } from 'express-handlebars';
+const app = express();
+const server = http.createServer(app);
+
+//set view engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static('./public'));
+const io = new Server(server);
+server.listen(3000, () => {
+    console.log('app dang chay')
+});
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} connected!!!`);
+
+    socket.on('send', (data) => {
+        //io.sockets.emit('Server-send-data', data);
+        //socket.emit('Server-send-data', data);
+        socket.broadcast.emit('Server-send-data', data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnected!!!`)
+    })
+})
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
---
+++ public/handleSocket.js
@@ -0,0 +1,7 @@
+const socket = io('http://localhost:3000');
+
+ 
+$(document).ready(() => {
+    $('#loginForm').show();
+    $('#chatForm').hide();
+})
