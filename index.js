60f60735ddd26e1a2af21ab5135270256d9bb646
hnhoa2000 <huynhngochoa2000@gmail.com>
2023-07-19 14:40:06 +0700

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { engine } from 'express-handlebars';
 const app = express();
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 //set view engine
@@ -12,7 +13,7 @@ app.set('views', './views');
 
 app.use(express.static('./public'));
 const io = new Server(server);
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('app dang chay')
 });
 
